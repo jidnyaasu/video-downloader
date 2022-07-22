8dcffc999c281036de77a1d975e678351ff9acf1
Saurabh <sbj.amb@gmail.com>
2022-07-22 12:13:57 +0530

    Used grid instead of place to position ui elements

--- video_downloader.py
+++ video_downloader.py
@@ -10,26 +10,24 @@ class DownloaderGui:
     def __init__(self, root, title):
         self.root = root
         self.root.title(title)
-        self.root.geometry("800x700+550+120")
         self.root.configure(bg="#FCF2D8")
+        self.root.config(padx=50, pady=20)
         
         # url textbox
         self.url_label = Label(self.root, text="Please paste Video Link here :  ", bg="#FCF2D8")
-        self.url_label.place(anchor=CENTER, relx=0.5, rely=0.05)
+        self.url_label.grid(column=0, row=0, columnspan=2, pady=(0, 5))
         self.url = Entry(self.root, width=50, borderwidth=3, bg="alice blue")
-        self.url.pack()
-        self.url.place(anchor=CENTER, relx=0.5, rely=0.15)
+        self.url.grid(column=0, row=1, columnspan=2, pady=5)
 
         # Paste button
         self.paste_button = Button(self.root, text="Paste", command=self.paste, bg="pink")
-        self.paste_button.place(anchor=CENTER, relx=0.5, rely=0.25)
+        self.paste_button.grid(column=0, row=2, columnspan=2, pady=5)
 
         # Output folder textbox
         self.output_folder_label = Label(self.root, text="Please select output folder :  ", bg="#FCF2D8")
-        self.output_folder_label.place(anchor=CENTER, relx=0.5, rely=0.35)
+        self.output_folder_label.grid(column=0, row=3, columnspan=2, pady=(20, 5))
         self.output_folder = Entry(self.root, width=50, borderwidth=3, bg="alice blue")
-        self.output_folder.pack()
-        self.output_folder.place(anchor=CENTER, relx=0.5, rely=0.45)
+        self.output_folder.grid(column=0, row=4, columnspan=2, pady=5)
 
         # Defaulting output folder to Videos directory
         if os.name == "nt":
@@ -40,7 +38,7 @@ class DownloaderGui:
 
         # Select folder button
         self.select_folder_button = Button(self.root, text="Select Folder", command=self.ask_folder, bg="pink")
-        self.select_folder_button.place(anchor=CENTER, relx=0.5, rely=0.55)
+        self.select_folder_button.grid(column=0, row=5, columnspan=2, pady=5)
 
         # Download buttons
         # Best quality button
@@ -49,8 +47,7 @@ class DownloaderGui:
             text="",
             bg="light green",
         )
-        self.best_button.pack()
-        self.best_button.place(anchor=CENTER, relx=0.35, rely=0.72)
+        self.best_button.grid(column=0, row=6, pady=25, padx=20, sticky="e")
 
         # mp4 format button
         self.mp4_button = Button(
@@ -58,20 +55,18 @@ class DownloaderGui:
             text="",
             bg="sky blue",
         )
-        self.mp4_button.pack()
-        self.mp4_button.place(anchor=CENTER, relx=0.65, rely=0.72)
+        self.mp4_button.grid(column=1, row=6, pady=25, padx=20, sticky="w")
 
         # Status Message
-        self.label = Label(self.root, text="", bg="#FCF2D8")
-        self.label.pack()
-        self.label.place(anchor=CENTER, relx=0.5, rely=0.85)
+        self.status_message = Label(self.root, text="", bg="#FCF2D8")
+        self.status_message.grid(column=0, row=7, pady=(5, 10), columnspan=2)
 
         # Author info
         font = Font(family="Helvetica", weight="bold")
         author = Label(
             self.root, text="Software by: Saurabh Joshi", fg="maroon", bg="#FCF2D8", font=font
         )
-        author.place(anchor=CENTER, relx=0.5, rely=0.95)
+        author.grid(column=0, row=8, columnspan=2, pady=(10, 0))
 
 
     def paste(self):
